4a0aed235ea3340ec469104183daf368a2570080
Tobias Lidskog <tobias@lidskog.se>
2017-04-22 12:16:25 +0200

    Decode params (#2)
    Updated decoding so '%26=1&%26=2&%26=%26' can be correctly decoded to {'&': [’1’, ’2’, ’&’]}.

--- background.js
+++ background.js
@@ -1,108 +1,79 @@
-((
+(function() {
   // make it work in both FF and Chrome
-  function() {
-    window.browser = (function() {
-      return window.msBrowser ||
-        window.browser ||
-        window.chrome;
-    })();
+  window.browser = window.msBrowser || window.browser || window.chrome;
 
-    // convert qyerystring to an array with keyAndValue
-    function getParameters(queryString) {
-      var decoded = decodeURIComponent(queryString);
-      var params = [];
-      var paramsAndValues = decoded.split('&');
-      for (var paramAndValue of paramsAndValues) {
-        var keyAndValue = paramAndValue.split('=');
-        params.push({
-          name: keyAndValue[0],
-          value: keyAndValue[1]
-        });
-      }
-      return params;
-    }
+  function parseQueryString(queryString) {
+    return queryString.split('&')
+      .map(pair => pair.split('='))
+      .map(([key, value]) => [decodeURIComponent(key), value !== undefined ? decodeURIComponent(value) : ''])
+      .reduce((map, [key, value]) => {
+        const values = map[key] || [];
+        values.push(value);
+        map[key] = values;
+        return map;
+      }, {});
+  }
 
-    function getAction(parameterArray) {
-      var action = {};
-      action.blocked = [];
-      action.requestHeaders = [];
+  function getActions(params) {
+    const blocked = (params.blocked || [])
+      .map(domain => `*://${domain}/*`);
 
-      for (var param of parameterArray) {
-        /// If it is a request header split and add it
-        if (param.name === 'rh') {
-          action.requestHeaders.push({
-            name: param.value.split(':')[0],
-            value: param.value.split(':').slice(1).join(':')
-          });
-        } else if (param.name === 'bl') {
-          // convert domain to block syntax
-          action.blocked.push('*://' + param.value + '/*');
-        }
-
-        if (param.name === 'domain') {
-          action.domain = param.value;
-        }
-
-        if (param.name === 'clear') {
-          action.clearCache = true;
-        }
-      }
-      return action;
-    }
-
-    browser.runtime.onMessage.addListener(
-
-      function(response, sender, sendResponse) {
-        var parameters = getParameters(response.queryString);
-        var action = getAction(parameters);
-
-        if (action.requestHeaders.length > 0) {
-
-          const domain = action.domain ? action.domain : "<all_urls>";
-
-          browser.webRequest.onBeforeSendHeaders.addListener(
-            function(details) {
-              for (var header of action.requestHeaders) {
-                details.requestHeaders.push({
-                  name: header.name,
-                  value: header.value
-                })
-              }
-              return {
-                requestHeaders: details.requestHeaders
-              };
-            }, {
-              urls: [domain]
-            }, ["blocking", "requestHeaders"]);
-
-        }
-
-        if (action.blocked.length > 0) {
-          browser.webRequest.onBeforeRequest.addListener(
-            function(details) {
-              return {
-                cancel: true
-              };
-            }, {
-              urls: action.blocked
-            }, ["blocking"]);
-        }
-
-        if (action.clearCache) {
-          var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-          var oneWeekAgo = (new Date()).getTime() - millisecondsPerWeek;
-          browser.browsingData.remove({
-            "since": oneWeekAgo
-          }, {
-            "appcache": true,
-            "cache": true,
-            "cookies": true,
-            "fileSystems": true,
-            "indexedDB": true,
-            "localStorage": true,
-            "serverBoundCertificates": true,
-            "serviceWorkers": true
-          }, function() {});
-        }
+    const requestHeaders = (params.rh || [])
+      .map(headerString => headerString.split(/:(.+)/))
+      .map(parts => {
+        return {key: parts[0], value: parts[1]};
       });
-  })());
+
+    const domain = params.domain;
+
+    const clearCache = !!params.clear;
+
+    return {blocked, requestHeaders, domain, clearCache};
+  }
+
+  browser.runtime.onMessage.addListener((message) => {
+    const params = parseQueryString(message.queryString);
+    const actions = getActions(params);
+
+    if (actions.requestHeaders.length > 0) {
+      const domain = actions.domain ? actions.domain : '<all_urls>';
+
+      browser.webRequest.onBeforeSendHeaders.addListener(details => {
+        details.requestHeaders.push(...actions.requestHeaders);
+        return {
+          requestHeaders: details.requestHeaders
+        };
+      }, {
+        urls: [domain]
+      }, ['blocking', 'requestHeaders']);
+    }
+
+    if (actions.blocked.length > 0) {
+      browser.webRequest.onBeforeRequest.addListener(() => {
+        return {
+          cancel: true
+        };
+      }, {
+        urls: actions.blocked
+      }, ['blocking']);
+    }
+
+    if (actions.clearCache) {
+      const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+      const oneWeekAgo = Date.now() - millisecondsPerWeek;
+      browser.browsingData.remove({
+        'since': oneWeekAgo
+      }, {
+        'appcache': true,
+        'cache': true,
+        'cookies': true,
+        'fileSystems': true,
+        'indexedDB': true,
+        'localStorage': true,
+        'serverBoundCertificates': true,
+        'serviceWorkers': true
+      }, function() {
+      });
+    }
+  });
+}());
