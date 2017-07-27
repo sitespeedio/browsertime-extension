c1432a031304c75b7a96ca6399670690537ffefc
soulgalore <peter@soulgalore.com>
2017-07-27 17:23:06 +0200

    make it work in FF

--- background.js
+++ background.js
@@ -3,11 +3,24 @@
   window.browser = window.msBrowser || window.browser || window.chrome;
 
   // Chrome specific code
-  if (window.chrome) {
+  if (
+    window.chrome &&
+    window.chrome.privacy &&
+    window.chrome.privacy.services
+  ) {
     // Inspired from WebPageTest https://github.com/WPO-Foundation/webpagetest/commit/a84e713ac1d9b7f0d78519dfbdc078e73b943b06
-    chrome.privacy.services.passwordSavingEnabled.set({ value: false }, function(){});
-    chrome.privacy.services.autofillEnabled.set({ value: false }, function(){});
-    chrome.privacy.services.translationServiceEnabled.set({ value: false }, function(){});
+    chrome.privacy.services.passwordSavingEnabled.set(
+      { value: false },
+      function() {}
+    );
+    chrome.privacy.services.autofillEnabled.set(
+      { value: false },
+      function() {}
+    );
+    chrome.privacy.services.translationServiceEnabled.set(
+      { value: false },
+      function() {}
+    );
   }
 
   // set Basic Auth credentials
@@ -28,33 +41,31 @@
 
     function provideCredentialsSync(requestDetails) {
       if (pendingRequests.indexOf(requestDetails.requestId) != -1) {
-        return { cancel:true };
+        return { cancel: true };
       }
       pendingRequests.push(requestDetails.requestId);
       return { authCredentials };
     }
 
     browser.webRequest.onAuthRequired.addListener(
-        provideCredentialsSync,
-        { urls: [url] },
-        ["blocking"]
-      );
-
-    browser.webRequest.onCompleted.addListener(
-      completed,
-      { urls: [url] }
+      provideCredentialsSync,
+      { urls: [url] },
+      ["blocking"]
     );
 
-    browser.webRequest.onErrorOccurred.addListener(
-      completed,
-      { urls: [url] }
-    );
+    browser.webRequest.onCompleted.addListener(completed, { urls: [url] });
+
+    browser.webRequest.onErrorOccurred.addListener(completed, { urls: [url] });
   }
 
   function parseQueryString(queryString) {
-    return queryString.split('&')
-      .map(pair => pair.split('='))
-      .map(([key, value]) => [decodeURIComponent(key), value !== undefined ? decodeURIComponent(value) : ''])
+    return queryString
+      .split("&")
+      .map(pair => pair.split("="))
+      .map(([key, value]) => [
+        decodeURIComponent(key),
+        value !== undefined ? decodeURIComponent(value) : ""
+      ])
       .reduce((map, [key, value]) => {
         const values = map[key] || [];
         values.push(value);
@@ -64,13 +75,12 @@
   }
 
   function getActions(params) {
-    const blocked = (params.bl || [])
-      .map(domain => `*://${domain}/*`);
+    const blocked = (params.bl || []).map(domain => `*://${domain}/*`);
 
     const requestHeaders = (params.rh || [])
       .map(headerString => headerString.split(/:(.+)/))
       .map(parts => {
-        return {name: parts[0], value: parts[1]};
+        return { name: parts[0], value: parts[1] };
       });
 
     const domain = params.domain;
@@ -79,64 +89,79 @@
 
     let basicAuth = undefined;
     if (params.ba) {
-      const parts =  (new String(params.ba)).split('@');
+      const parts = new String(params.ba).split("@");
       basicAuth = {
         username: parts[0],
         password: parts[1],
         url: parts[2]
-      }
+      };
     }
 
-    return {blocked, requestHeaders, domain, clearCache, basicAuth};
+    return { blocked, requestHeaders, domain, clearCache, basicAuth };
   }
 
-  browser.runtime.onMessage.addListener((message) => {
+  browser.runtime.onMessage.addListener(message => {
     const params = parseQueryString(message.queryString);
     const actions = getActions(params);
 
     if (actions.requestHeaders.length > 0) {
-      const domain = actions.domain ? actions.domain : '<all_urls>';
+      const domain = actions.domain ? actions.domain : "<all_urls>";
 
-      browser.webRequest.onBeforeSendHeaders.addListener(details => {
-        details.requestHeaders.push(...actions.requestHeaders);
-        return {
-          requestHeaders: details.requestHeaders
-        };
-      }, {
-        urls: [domain]
-      }, ['blocking', 'requestHeaders']);
+      browser.webRequest.onBeforeSendHeaders.addListener(
+        details => {
+          details.requestHeaders.push(...actions.requestHeaders);
+          return {
+            requestHeaders: details.requestHeaders
+          };
+        },
+        {
+          urls: [domain]
+        },
+        ["blocking", "requestHeaders"]
+      );
     }
 
     if (actions.blocked.length > 0) {
-      browser.webRequest.onBeforeRequest.addListener(() => {
-        return {
-          cancel: true
-        };
-      }, {
-        urls: actions.blocked
-      }, ['blocking']);
+      browser.webRequest.onBeforeRequest.addListener(
+        () => {
+          return {
+            cancel: true
+          };
+        },
+        {
+          urls: actions.blocked
+        },
+        ["blocking"]
+      );
     }
 
     if (actions.basicAuth) {
-      setupBasicAuth(actions.basicAuth.username, actions.basicAuth.password, actions.basicAuth.url);
+      setupBasicAuth(
+        actions.basicAuth.username,
+        actions.basicAuth.password,
+        actions.basicAuth.url
+      );
     }
 
     if (actions.clearCache) {
       const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
       const oneWeekAgo = Date.now() - millisecondsPerWeek;
-      browser.browsingData.remove({
-        'since': oneWeekAgo
-      }, {
-        'appcache': true,
-        'cache': true,
-        'cookies': true,
-        'fileSystems': true,
-        'indexedDB': true,
-        'localStorage': true,
-        'serverBoundCertificates': true,
-        'serviceWorkers': true
-      }, function() {
-      });
+      browser.browsingData.remove(
+        {
+          since: oneWeekAgo
+        },
+        {
+          appcache: true,
+          cache: true,
+          cookies: true,
+          fileSystems: true,
+          indexedDB: true,
+          localStorage: true,
+          serverBoundCertificates: true,
+          serviceWorkers: true
+        },
+        function() {}
+      );
     }
   });
-}());
+})();
