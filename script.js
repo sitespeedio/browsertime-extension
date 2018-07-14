25ff224ddd15cc0a798043e3ca99036f41ed0ee0
Peter Hedenskog <peter@soulgalore.com>
2018-07-14 21:16:30 +0200

    Cleanup (#7)
    * Cleanup adn fix specific FF/Chrome code
    
    * new version

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-
 ((function() {
-window.browser = (function () {
-  return window.msBrowser ||
-    window.browser ||
-    window.chrome;
-})();
-  if (window.chrome) {
-    browser.runtime.sendMessage({queryString: window.location.search.slice(1)}, function() {});
+  // Both Chrome and Firefox has window.chrome (since FF 55)
+  // That could be a good solution ... if FF implemented everything the same as Chrome
+  // but we use functions that differs, so we need to keep track of them.
+  // In general: Firefox return promises, Chrome take a function as last param
+  const isChrome = !window.browser;
+  if (isChrome) {
+    chrome.runtime.sendMessage({queryString: window.location.search.slice(1)}, function() {});
   } 
   else {
     // Something changed in FF 55
-    browser.runtime.sendMessage({queryString: window.location.search.slice(1)});
+    async function send() {
+      await browser.runtime.sendMessage({queryString: window.location.search.slice(1)});
+    } 
+    send();
   }
- 
 })());
